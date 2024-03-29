8dedda9ee9f33f4eac945fdb4e46b919b4ee0221
angelorange <acostalarangeira@gmail.com>
2024-03-28 22:17:45 -0300

    tests in wip

--- test/shannon_entropy_test.exs
+++ test/shannon_entropy_test.exs
@@ -1,8 +1,16 @@
 defmodule ShannonEntropyTest do
   use ExUnit.Case
-  doctest ShannonEntropy
 
-  test "greets the world" do
-    assert ShannonEntropy.hello() == :world
+  alias ShannonEntropy
+
+  test "calculates Shannon entropy correctly" do
+    data = ["A", "R", "A", "R", "A"]
+    expected_counts = [%{"A" => 2}, %{"R" => 2}]
+    expected_entropy = 0.9709505944546686
+
+    {category_counts, entropy} = ShannonEntropy.run(data)
+
+    assert category_counts == expected_counts
+    assert Float.round(entropy, 10) == Float.round(expected_entropy, 10)
   end
 end
